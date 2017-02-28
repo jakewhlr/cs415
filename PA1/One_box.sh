9464739f9f8200227bb3c694d3f61c36524120bb
Jake Wheeler <jakewheeler@nevada.unr.edu>
2017-02-27 21:07:49 -0800

    updated sbatch files

--- PA1/One_box.sh
+++ PA1/One_box.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SBATCH -n 2
 #SBATCH -N 1
+#SBATCH --output=./One_box.txt
 #SBATCH --time=00:15:00
 srun PA1 1000 1
--- PA1/Two_box.sh
+++ PA1/Two_box.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH -n 2
 #SBATCH -N 2
+#SBATCH --output=./Two_box.txt
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=00:15:00
 srun PA1 1000 1
