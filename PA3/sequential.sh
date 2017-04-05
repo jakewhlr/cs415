904c1c6d8bd5e3f95e9aaf40aea9a961cb486ac3
Jake Wheeler <jakewhlr@gmail.com>
2017-04-04 21:37:18 -0700

    fixed error in sequential.sh

--- PA3/sequential.sh
+++ PA3/sequential.sh
@@ -26,19 +26,19 @@ printf "$out," >> ./output/sequential/sequential.csv && printf " $out s\n"
 
 printf "1,000,000,000 integers"
 out=$(srun bin/sequential -c -s 1000000000 -e 123)
-printf "$out\n" >> ./output/sequential/sequential.csv && printf " $out s\n"
+printf "$out," >> ./output/sequential/sequential.csv && printf " $out s\n"
 
 printf "1,250,000,000 integers"
 out=$(srun bin/sequential -c -s 1250000000 -e 123)
-printf "$out\n" >> ./output/sequential/sequential.csv && printf " $out s\n"
+printf "$out," >> ./output/sequential/sequential.csv && printf " $out s\n"
 
 printf "1,500,000,000 integers"
 out=$(srun bin/sequential -c -s 1500000000 -e 123)
-printf "$out\n" >> ./output/sequential/sequential.csv && printf " $out s\n"
+printf "$out," >> ./output/sequential/sequential.csv && printf " $out s\n"
 
 printf "1,750,000,000 integers"
 out=$(srun bin/sequential -c -s 1750000000 -e 123)
-printf "$out\n" >> ./output/sequential/sequential.csv && printf " $out s\n"
+printf "$out," >> ./output/sequential/sequential.csv && printf " $out s\n"
 
 printf "2,000,000,000 integers"
 out=$(srun bin/sequential -c -s 2000000000 -e 123)
