a8a5a55b854859ae50f7d62da528c37dba91a108
Jake Wheeler <jakewheeler@nevada.unr.edu>
2017-04-27 01:04:29 -0700

    added mpi=pmi2 to parallel.sh

--- PA4/parallel.sh
+++ PA4/parallel.sh
@@ -9,37 +9,37 @@ out=$(srun bin/parallel -c -s 60 -e 123)
 printf "$out," >> ./output/parallel/parallel.csv && printf " $out s,"
 
 printf "120x120"
-out=$(srun bin/parallel -c -s 120 -e 123)
+out=$(srun --mpi=pmi2 bin/parallel -c -s 120 -e 123)
 printf "$out," >> ./output/parallel/parallel.csv && printf " $out s,"
 
 printf "240x240"
-out=$(srun bin/parallel -c -s 240 -e 123)
+out=$(srun --mpi=pmi2 bin/parallel -c -s 240 -e 123)
 printf "$out," >> ./output/parallel/parallel.csv && printf " $out s,"
 
 printf "300x300"
-out=$(srun bin/parallel -c -s 300 -e 123)
+out=$(srun --mpi=pmi2 bin/parallel -c -s 300 -e 123)
 printf "$out," >> ./output/parallel/parallel.csv && printf " $out s,"
 
 printf "420x420"
-out=$(srun bin/parallel -c -s 420 -e 123)
+out=$(srun --mpi=pmi2 bin/parallel -c -s 420 -e 123)
 printf "$out," >> ./output/parallel/parallel.csv && printf " $out s,"
 
 printf "540x540"
-out=$(srun bin/parallel -c -s 540 -e 123)
+out=$(srun --mpi=pmi2 bin/parallel -c -s 540 -e 123)
 printf "$out," >> ./output/parallel/parallel.csv && printf " $out s,"
 
 printf "660x660"
-out=$(srun bin/parallel -c -s 660 -e 123)
+out=$(srun --mpi=pmi2 bin/parallel -c -s 660 -e 123)
 printf "$out\n" >> ./output/parallel/parallel.csv && printf " $out s,"
 
 printf "780x780"
-out=$(srun bin/parallel -c -s 780 -e 123)
+out=$(srun --mpi=pmi2 bin/parallel -c -s 780 -e 123)
 printf "$out\n" >> ./output/parallel/parallel.csv && printf " $out s,"
 
 printf "900x900"
-out=$(srun bin/parallel -c -s 900 -e 123)
+out=$(srun --mpi=pmi2 bin/parallel -c -s 900 -e 123)
 printf "$out\n" >> ./output/parallel/parallel.csv && printf " $out s,"
 
 printf "1020x1020"
-out=$(srun bin/parallel -c -s 1020 -e 123)
+out=$(srun --mpi=pmi2 bin/parallel -c -s 1020 -e 123)
 printf "$out\n" >> ./output/parallel/parallel.csv && printf " $out s\n"
