8ebbd4ed4826751d55cfa8843bb649ca65067de6
jakewhlr <jakewheeler@nevada.unr.edu>
2017-04-27 00:28:37 -0700

    Update parallel.sh

--- PA4/parallel.sh
+++ PA4/parallel.sh
@@ -4,30 +4,42 @@
 #SBATCH --mail-user=jakewheeler@nevada.unr.edu
 #SBATCH --mail-type=ALL
 #SBATCH --time=00:20:00
-printf "100x100"
-out=$(srun bin/parallel -c -s 100 -e 123)
-printf "$out," >> ./output/parallel/parallel.csv && printf " $out s\n"
+printf "60x60"
+out=$(srun bin/parallel -c -s 60 -e 123)
+printf "$out," >> ./output/parallel/parallel.csv && printf " $out s,"
 
-printf "500x500"
-out=$(srun bin/parallel -c -s 500 -e 123)
-printf "$out," >> ./output/parallel/parallel.csv && printf " $out s\n"
+printf "120x120"
+out=$(srun bin/parallel -c -s 120 -e 123)
+printf "$out," >> ./output/parallel/parallel.csv && printf " $out s,"
 
-printf "1000x1000"
-out=$(srun bin/parallel -c -s 1000 -e 123)
-printf "$out," >> ./output/parallel/parallel.csv && printf " $out s\n"
+printf "240x240"
+out=$(srun bin/parallel -c -s 240 -e 123)
+printf "$out," >> ./output/parallel/parallel.csv && printf " $out s,"
 
-printf "1500x1500"
-out=$(srun bin/parallel -c -s 1500 -e 123)
-printf "$out," >> ./output/parallel/parallel.csv && printf " $out s\n"
+printf "300x300"
+out=$(srun bin/parallel -c -s 300 -e 123)
+printf "$out," >> ./output/parallel/parallel.csv && printf " $out s,"
 
-printf "2000x2000"
-out=$(srun bin/parallel -c -s 2000 -e 123)
-printf "$out," >> ./output/parallel/parallel.csv && printf " $out s\n"
+printf "420x420"
+out=$(srun bin/parallel -c -s 420 -e 123)
+printf "$out," >> ./output/parallel/parallel.csv && printf " $out s,"
 
-printf "2500x2500"
-out=$(srun bin/parallel -c -s 2500 -e 123)
-printf "$out," >> ./output/parallel/parallel.csv && printf " $out s\n"
+printf "540x540"
+out=$(srun bin/parallel -c -s 540 -e 123)
+printf "$out," >> ./output/parallel/parallel.csv && printf " $out s,"
 
-printf "3000x3000"
-out=$(srun bin/parallel -c -s 3000 -e 123)
+printf "660x660"
+out=$(srun bin/parallel -c -s 660 -e 123)
+printf "$out\n" >> ./output/parallel/parallel.csv && printf " $out s,"
+
+printf "780x780"
+out=$(srun bin/parallel -c -s 780 -e 123)
+printf "$out\n" >> ./output/parallel/parallel.csv && printf " $out s,"
+
+printf "900x900"
+out=$(srun bin/parallel -c -s 900 -e 123)
+printf "$out\n" >> ./output/parallel/parallel.csv && printf " $out s,"
+
+printf "1020x1020"
+out=$(srun bin/parallel -c -s 1020 -e 123)
 printf "$out\n" >> ./output/parallel/parallel.csv && printf " $out s\n"
